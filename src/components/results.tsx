d19bb28b03f80e3423cf293d3a6c45f75040b204
JordanReady <jready112@gmail.com>
2023-06-08 12:55:07 -0500

    setting up summary and skill containers

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -3,20 +3,40 @@ import "./results.css";
 function Results() {
   return (
     <div className="results-container">
-      <div className="your-result-container">
-        <h1 className="your-result-title">Your Result</h1>
+      <div className="result-container">
+        <h1 className="result-title">Your Result</h1>
         <div className="circle-score-container">
           <span className="circle-score">76</span>
           <p className="circle-score-text">of 100</p>
         </div>
-        <div className="your-result-rating-container">
-          <h2 className="your-result-rating">Great</h2>
-          <p className="your-result-description">
+        <div className="result-rating-container">
+          <h2 className="result-rating">Great</h2>
+          <p className="result-description">
             You scored higher than 65% of the people who have taken these tests.
           </p>
         </div>
       </div>
-      <div className="summary-container"></div>
+      <div className="summary-container">
+        <h1 className="summary-title">Summary</h1>
+        <div className="skill-container reaction">
+          <span className="top-left-corner-accent corner-accent"></span>
+          <span className="top-right-corner-accent corner-accent"></span>
+          <span className="bottom-left-corner-accent corner-accent"></span>
+          <span className="bottom-right-corner-accent corner-accent"></span>
+          <div className="skill-type">
+            <img
+              src="src\assets\icon-reaction.svg"
+              alt="reaction icon"
+              className="skill-icon"
+            />
+            <p className="skill-title">Reaction</p>
+          </div>
+          <div className="skill-score">
+            <span className="your-score">80</span>
+            <span className="total-score"> / 100</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
