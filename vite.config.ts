83c308e4f2f09497888d1b53d83a9e03b6126a56
JordanReady <jready112@gmail.com>
2023-06-09 21:11:55 -0500

    github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/results-summary-component",
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        dir: "build",
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+});
