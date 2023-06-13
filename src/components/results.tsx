aabc830e21cde5251a99b2203fad83edc9212264
JordanReady <jready112@gmail.com>
2023-06-13 13:08:48 -0500

    cleaning assests file

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -9,10 +9,10 @@ function Results() {
   const [circleScore, setCircleScore] = useState(0);
   const [resultRating, setResultRating] = useState("Great");
   const [resultPercentage, setResultPercentage] = useState(0);
-  const [reactionScore, setReactionScore] = useState<number | null>(0);
-  const [memoryScore, setMemoryScore] = useState<number | null>(0);
-  const [verbalScore, setVerbalScore] = useState<number | null>(0);
-  const [visualScore, setVisualScore] = useState<number | null>(0);
+  const [reactionScore, setReactionScore] = useState(0);
+  const [memoryScore, setMemoryScore] = useState(0);
+  const [verbalScore, setVerbalScore] = useState(0);
+  const [visualScore, setVisualScore] = useState(0);
   const [data, setData] = useState<Array<{ scores: Array<{ score: number }> }>>(
     []
   );
